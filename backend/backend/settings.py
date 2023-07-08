989b8ad62b055d691db949c07f9cf83290cfe167
yıldız <mesel4461@gmail.com>
2023-07-08 14:57:47 +0300

    as

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo',
+    'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -99,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CORS_ORIGIN_WHITELIST =('http://localhost:3000')
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
